84923ab49bd45e7819e9bec811a3beaded6e6a1a
Samuel Goebert <hello@bigcurl.de>
2013-05-10 09:10:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OaiRegistryDistributed::Application.config.secret_token = 'e3e4c8e6fc3994501346f9403b5ac1d783ce28e40ae8f8842dc5c182224d2ed3543a29befdf7a163ec571cab7b79f7d765a3cf5ab59652c2ed65aabcadda87b5'
